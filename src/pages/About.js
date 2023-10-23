2b83bf3f176af9e65418a3007c236292455e0422
mey2023 <it.reksmey@gmail.com>
2023-10-23 19:59:21 +0700

    change in about by reksmey

--- src/pages/About.js
+++ src/pages/About.js
@@ -27,7 +27,9 @@ export default function About() {
             href="#"
             className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
           >
-            Get started
+            Get started to teach reactjs with github 
+            Hello happy birthday - by Reksmey
+
             <svg
               className="ml-2 -mr-1 w-5 h-5"
               fill="currentColor"
