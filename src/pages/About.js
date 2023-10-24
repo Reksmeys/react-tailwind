718e02bd461d72729359c7d216ea4ec066f5929d
Lalaphalla <lalaphalla@gmail.com>
2023-10-24 18:20:15 +0700

    change start again about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -27,7 +27,7 @@ export default function About() {
             href="#"
             className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
           >
-            Get started
+            Let Start again
             <svg
               className="ml-2 -mr-1 w-5 h-5"
               fill="currentColor"
